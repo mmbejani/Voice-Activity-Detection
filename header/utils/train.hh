e6d7029b84cbf7134b073dbfe8b42cdc499bf8d3
Mahdi <mohammad.mahdi.bejani@gmail.com>
2023-02-08 21:03:39 +0330

    start to implement the train process

--- header/utils/train.hh
+++ header/utils/train.hh
@@ -7,6 +7,27 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
-DEFINE_string(model_config, "model.conf", "Path to Config of Model");
+#include <dataset.hh>
 
-DEFINE_uint32(num_feature, 13, "Number of extracted features");
+
+namespace za{
+    DECLARE_string(model_config);
+    DECLARE_uint32(num_feature);
+
+    class Train {
+
+        public:
+            Train(std::shared_ptr<fl::Sequential>& model, 
+                  std::unique_ptr<VADDataset>& dataset,
+                  std::unique_ptr<fl::CategoricalCrossEntropy>& loss_function,
+                  std::unique_ptr<fl::FirstOrderOptimizer>& optimizer,
+                  const uint16_t max_epochs);
+
+            void start_train_process();
+
+        private:
+            void step();
+            void start_of_epoch();
+            void end_of_epoch();
+    };
+}
--- src/utils/train.cc
+++ src/utils/train.cc
@@ -4,6 +4,34 @@
 using fl::pkg::runtime::buildSequentialModule;
 using fl::lib::audio::Mfcc;
 using fl::lib::audio::FeatureParams;
+namespace za{
+    DEFINE_string(model_config, "model.conf", "Path to Config of Model");
+    DEFINE_uint32(num_feature, 13, "Number of extracted features");
+
+    Train::Train(std::shared_ptr<fl::Sequential>& model, 
+                  std::unique_ptr<VADDataset>& dataset,
+                  std::unique_ptr<fl::CategoricalCrossEntropy>& loss_function,
+                  std::unique_ptr<fl::FirstOrderOptimizer>& optimizer,
+                  const uint16_t max_epochs){
+                    
+                  }
+
+    void Train::start_train_process(){
+
+    }
+
+    void Train::step(){
+
+    }
+
+    void Train::start_of_epoch(){
+
+    }
+
+    void Train::end_of_epoch(){
+
+    }
+}
 
 int main(int argc, char **argv){
     fl::init();
@@ -13,14 +41,13 @@ int main(int argc, char **argv){
     gflags::ParseCommandLineFlags(&argc, &argv, false);
 
     LOG(INFO) << "Loading Model ...";
-    auto model = buildSequentialModule(FLAGS_model_config, FLAGS_num_feature, 2);
+    auto model = buildSequentialModule(za::FLAGS_model_config, za::FLAGS_num_feature, 2);
     
     LOG(INFO) << "Creating Feature Params ...";
     auto featureParam = FeatureParams();
     auto mfcc = std::make_shared<Mfcc>(featureParam);
 
     LOG(INFO) << "Creating Dataset ...";
-    za::VADDataset dataset("");
     
     return 0;
 }

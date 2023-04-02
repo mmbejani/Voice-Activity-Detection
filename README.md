0bf64de1eaa31f586a1f33a36f639a03777044cb
mmbejani <mohammad.mahdi.bejani@gmail.com>
2023-04-02 18:19:21 +0330

    make README file more confident!

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Voice Activity Detection (Human Voice Detection)
-This library has been implemented for voice activity detection (VAD) by human voice detection aspect. Therefore, the model is classified voices into two class, no-human (noise) and human voice. The library is based on C/C++17 (Clang preferred).
+# Speech Anomaly Detection (Normal Human Voice Detection)
+This library has been implemented for voice activity detection (VAD) by human voice detection aspect. Therefore, the model is classified voices into two class, no-human (noise) and human voice. The library is based on C/C++17 (Clang compiler is preferred).
+
+
+# Pre-requirements
+This library is highly depends on Flashlight library (url!!!!!!). Then take to following step to install it:
+ 1) Install libraries `boost`, `glog`, `gflag`, and `json`, 
+ 2) Install Flashlight from following link (version 0.3):
+
+ 3) Install `GRPC` to build it as service.
+
+# How to install
+Run following commands to build executable file:
+```
+git clone https://github.com/mmbejani/Voice-Activity-Detection
+
+cd Voice-Activity-Detection
+
+mkdir build && cd build
+
+cmake .. [OPTION]
+
+make -j 2
+
+./run_[corrosponding built file]
+```
+
+Instead of `OPTION` use the following option 
+
+
+
+
+
+
+
+choose between `-DTRAIN=ON` to build project for training and `-DINFER=ON` to build project for inference on local data,. Beside, corresponding to building option in the last step a run file is maked. `
--- header/inference.hh
+++ header/inference.hh
@@ -3,6 +3,8 @@
 #include <flashlight/fl/flashlight.h>
 #include <flashlight/pkg/runtime/common/SequentialBuilder.h>
 
+#include <fstream>
+
 #include "vad.hh"
 
 using namespace std;
@@ -18,10 +20,40 @@ namespace za{
     public:
         Inference(const string&);
 
+        //For inferencing from a single utterance file
         fl::Variable infer(const string&);
+
+        //For inferencing from single stream of utterance bytes
+        fl::Variable infer(const ifstream&);
+
+        //For inferencing from single tensor respect to an utterance
         fl::Variable infer(const fl::Variable&);
         
+        //Inference on some utterances as file paths (batch manner)
         fl::Variable inferBatch(const vector<string>&);
-        fl::Variable inferBatch(const fl::Variable&);
+
+        //Inference on some utterances as byte stream (batch manner)
+        fl::Variable inferBatch(const vector<ifstream>&);
+
+        /**Inference on some utterances a tensor (batch manner) where
+          the shape of the tensor is BxL (B and L stand for batch-size and
+          max length of utterances).
+
+          @param input_signals: It is a flashlight Variable which
+                                carries out a tensor of utterace signal
+          @param input_lengths: It is a flashlight Variable which
+                                carries out the lengths of signals of the 
+                                first param.
+
+          @return this method return a vector with length B which is $\{0,1\}^B$ 
+                  where B is batch-size and one stands for normal human utterance
+                  and zero represents abnormal utterance or voice (The defination is
+                  different from VAD. We name it SAD or Speech Anomaly Detection).
+                  Zarebin Assistant members know the meaning of SAD. To become familiar
+                  with the concept be in touch with ZA squad ;).
+          
+        */
+        fl::Variable inferBatch(const fl::Variable& input_signals, 
+                                const fl::Variable& input_lengths);
     };   
 }
